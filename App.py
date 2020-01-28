c149b35e6eaec7bc9680691d604c05943a1e5838
Limei Hou <limei_hou@hotmail.com>
2020-01-28 17:08:45 -0500

    Prediction is done

---
+++ App.py
@@ -0,0 +1,168 @@
+## Flask
+import os
+import pandas as pd
+import numpy as np
+# from flask import Flask, jsonify, render_template
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request)
+
+
+from sklearn.metrics import accuracy_score 
+from sklearn.externals import joblib
+# from flask_sqlalchemy import SQLAlchemy
+# import sqlalchemy
+# from sqlalchemy.ext.automap import automap_base
+# from sqlalchemy.orm import Session
+# from sqlalchemy import create_engine
+
+app = Flask(__name__ , static_url_path='')
+
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.preprocessing import MinMaxScaler
+
+
+number_of_features=15
+def create_network(optimizer='rmsprop'):   
+    network = models.Sequential()    
+    network.add(layers.Dense(units=16, activation='relu', input_shape=(number_of_features,))) 
+    network.add(layers.Dense(units=2, activation='softmax'))        
+    network.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])             
+    return network
+# ValueError: Error when checking input: expected dense_112_input to have shape (15,) but got array with shape (1,)
+# sloved by : test_array = np.asarray(test_inputs, dtype=np.float32)
+
+with open('pickles/gridlr.pkl', 'rb') as f:
+    gridlr = joblib.load(f)
+with open('pickles/gridsvm.pkl', 'rb') as f:
+    gridsvm = joblib.load(f)
+with open('pickles/griddeep.pkl', 'rb') as f:
+    griddeep = joblib.load(f)
+    
+gridmodels = {'Logistic Regression': gridlr,
+              'Linear Support Vector Machine': gridsvm,
+              'Deep Neural Network Learning': griddeep
+             }
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/prediction")
+def prediction():
+    return render_template("modelprediction.html")
+@app.route("/development")
+def development():
+    return render_template("modeldevelopment.html")
+@app.route("/introduction")
+def introduction():
+    return render_template("modelintroduction.html")
+
+
+@app.route("/predictonesample", methods=["GET", "POST"])    # flask request: bring data back to Form by render jinja template
+# @app.route("/predictonesample/<inpdata>")
+def predictioninput():  
+    #<input>  15 columns
+    finalresult  = ""   
+    msg = ""
+    # print(request.method)                                           # initialization here for return "" for GET
+    if request.method =="POST":
+        # 1)dummpy : dropdowns(cooupation, applyloanreason): selected=1 unselected=0  
+        delinquentcreditlinescount = float(request.form["delinquentcreditlinescount"])  # is key not function form["inputdata2"] form("inputdata2")
+        derogatoryreportscount = float(request.form["derogatoryreportscount"])  
+        recentcreditinquirycount = float(request.form["recentcreditinquirycount"])  
+        debttoincomeratio = float(request.form["debttoincomeratio"])
+        creditlinecount = float(request.form["creditlinecount"])
+        currentpropertyvalue = float(request.form["currentpropertyvalue"]) 
+        mortgageamountdue = float(request.form["mortgageamountdue"])
+        yearsatpresentjob = float(request.form["yearsatpresentjob"])        
+        # occupation = request.form["occupations"]  # Bad Request -- The browser (or proxy) sent a request that this server could not understand.
+        occupation = request.form.get('occupations')
+        occupation_Other = 0
+        occupation_Sales = 0
+        occupation_Self = 0
+        occupation_Mgr = 0
+        occupation_ProfExe = 0
+        if occupation == "Sales":
+            occupation_Sales = 1
+        elif occupation == "ProfExe":
+            occupation_ProfExe = 1
+        elif occupation == "Manager":
+            occupation_Mgr = 1
+        elif occupation == "Other":
+            occupation_Other = 1
+        elif occupation == "Self":
+            occupation_Self = 1
+        else:
+            msg = msg + "Occupation is not selected! "
+            # print("not select occupation")
+        # applyloanreason = request.form["applyloanfeasons"]
+        applyloanreason = request.form.get('applyloanreasons')
+        applyloanreason_DebtCon = 0
+        applyloanreason_HomeImp = 0
+        if applyloanreason == "Debt Consolidation":
+            applyloanreason_DebtCon = 1
+        elif applyloanreason == "Home Improvement":  # HomeImp
+            applyloanreason_HomeImp = 1
+        else:
+            msg = msg + "Reason is not selected! "
+            # print("not select reason")
+
+        test_inputs = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+        # below order can't be changed 
+        test_inputs[0][0] = delinquentcreditlinescount  
+        test_inputs[0][1] = derogatoryreportscount  
+        test_inputs[0][2] = recentcreditinquirycount  
+        test_inputs[0][3] = debttoincomeratio  
+        test_inputs[0][4] = occupation_Other  
+        test_inputs[0][5] = occupation_Sales  
+        test_inputs[0][6] = occupation_Self  
+        test_inputs[0][7] = applyloanreason_HomeImp  
+        test_inputs[0][8] = occupation_Mgr  
+        test_inputs[0][9] = creditlinecount  
+        test_inputs[0][10] = currentpropertyvalue   
+        test_inputs[0][11] = applyloanreason_DebtCon  
+        test_inputs[0][12] = occupation_ProfExe  
+        test_inputs[0][13] = mortgageamountdue  
+        test_inputs[0][14] = yearsatpresentjob  
+
+        test_array = np.asarray(test_inputs, dtype=np.float32)
+
+        # 2) MinMaxScaler(): dummied fields + other integer fields    
+        X_train = pd.read_csv("data\HomeEquityLoans_X_train.csv")  # should from DB??
+        X_scaler = MinMaxScaler().fit(X_train)
+        test_array_scaled = X_scaler.transform(test_array)
+        # print(test_array_scaled )
+
+        gridlr_predictions = gridlr.predict(test_array_scaled)   # error 500: Failed to load resource: the server responded with a status of 500 (INTERNAL SERVER ERROR)
+        gridsvm_predictions = gridsvm.predict(test_array_scaled)
+        griddeep_predictions = griddeep.predict(test_array_scaled)
+
+        all_predictions = zip(gridlr_predictions, gridsvm_predictions, griddeep_predictions)
+
+        final_predictions = []    
+        for tup in all_predictions:
+            final_predictions.append( max( set(list(tup)), key=list(tup).count ) )  
+            final=final_predictions[0]
+
+        if final == 1:
+            finalresult = "Default"
+        else:
+            finalresult = "Not Default" 
+
+    print(msg)
+    return render_template("modelprediction.html", prediction = finalresult, msg=msg )
+
+# @app.route('/modeldevelopement/')
+# def prediction():                                          
+#     # <4> get the static graph picutre from AWS S3 bucket - save in jupyter notevbook and stored in AWS S3
+#     # <5> use plotly danamically genertate graph
+#     return "picture"
+
+if __name__ == '__main__':
+    app.run()
+
+
--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# Portfolio
-Personal Website
+
+
+
+# Home Equity Loan PD model
+
+   < In Development > 
+
+This project used <span style="color:purple;">**Machine Learning, Flask, HTML, CSS, Bootstrap**</span> to predict Home Equity Loan's probability of default. After user input data user will seee the perdiction result by rendering the templates with the model prediction result from running the machine learning model on server which uses <span style="color:purple;">**Jinja Template**</span> library on a HTML page. 
+
+- - -
+
+## Data Set
+
+The data set reports characteristics and delinquency information for 5,960 home equity loans. 
+
+A home equity loan is a loan where the obligor uses the equity of his or her home as the underlying collateral. The data set has the following characteristics:
+
+◾ BAD: 1 = applicant defaulted on loan or seriously delinquent; 0 = applicant paid loan
+
+◾ LOAN: Amount of the loan request
+
+◾ MORTDUE: Amount due on existing mortgage
+
+◾ VALUE: Value of current property
+
+◾ REASON: DebtCon = debt consolidation; HomeImp = home improvement
+
+◾ JOB: Occupational categories
+
+◾ YOJ: Years at present job
+
+◾ DEROG: Number of major derogatory reports
+
+◾ DELINQ: Number of delinquent credit lines
+
+◾ CLAGE: Age of oldest credit line in months
+
+◾ NINQ: Number of recent credit inquiries
+
+◾ CLNO: Number of credit lines
+
+◾ DEBTINC: Debt-to-income ratio
+
+
+## Technologies Used
+
+*  **Bootstrap Table** is used to set the layout of the webpage and it can makes a *Responsive Tables* by using `table-responsive` class. It scrolls horizontally up to small devices (under 768px) and when viewing on anything larger than 768px wide no any difference.
+
+* Python data manipulation and analysis library **Pandas** is used for importing table data from a webpage adn convert the data to a HTML table string. 
+
+* Python web framework **Flask** and it's extensions **Render_Template** and **Redirect** are used to establish database connection,  to render templates with specific data by using Jinja template library, redirect route back to home page.
+
+* Python machine learning algrithems  **Logistic Regression** and **SVM** are used.
+
+## Project Files:
+
+* **DevHomeEquityPDModel.ipynb**: This is the script for building the machine learning model.
+
+* **app.py**: It is the Flask Server which has two routes. The root route `/` that will show the landing page, the other route called `/prediction` that will prvoide the default prediction API.
+
+* **index.html**: It is `Jinja` template HTML file structured by using `bootstrap`, it takes the prediction result string and displays all of the data in the appropriate HTML elements.
+
+* **modelprediction.html**: It is the page accept user's input and show the predictio result.
+
+## Final Results
+
+By sending requests from **Brower** to the **Flask Server** can get below results: 
+
+- - -
+
+
+
+
+- - -
